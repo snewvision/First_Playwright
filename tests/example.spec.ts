c70c4bf6d2d546660624358fd342f903658c7272
ShubhamN NewVision <shubham.nigam@newvision-software.com>
2026-04-08 11:45:29 +0530

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,7 +17,5 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 
-  
-  //Changes 1
-    changes 223
+  xyz
 });
